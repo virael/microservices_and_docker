caf619f8ea437b83dbbbbee9c52ff48d4a34ef67
ljanusz <reno19877@gmail.com>
2020-08-28 09:50:07 +0200

    added jpa dependency

--- student-app/src/main/java/com/ljanusz/student/domain/Student.java
+++ student-app/src/main/java/com/ljanusz/student/domain/Student.java
@@ -2,11 +2,20 @@ package com.ljanusz.student.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @AllArgsConstructor
 @Getter
+@Setter
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     private String name;
